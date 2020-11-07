7fce51aceece574163416c0483e34cad9a6924a8
James <JamesFox9467@gmail.com>
2020-11-07 15:54:22 -0500

    Added @EventHandler

--- Apricot/src/io/github/itzjamez/apricot/events/EventPlayerJoin.java
+++ Apricot/src/io/github/itzjamez/apricot/events/EventPlayerJoin.java
@@ -1,11 +1,13 @@
 package io.github.itzjamez.apricot.events;
 
 import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
 
 public class EventPlayerJoin implements Listener
 {
+    @EventHandler
     public void join(PlayerJoinEvent e)
     {
         Player p = e.getPlayer();
